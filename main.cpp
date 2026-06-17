1a052da67e998457826549a72dd908f035ede0b3
ByteMe6 <120343461+ByteMe6@users.noreply.github.com>
2026-06-18 01:41:39 +0300

    refactor: update main.cpp to use tree.h

--- main.cpp
+++ main.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include "treeStruct.h"
+#include "tree.h"
 
 int main() {
-    Tree *tree = new Tree();
-    tree->root = 0;
-    tree->left = nullptr;
-    tree->right = nullptr;
+    Tree *tree = new Tree{0, nullptr, nullptr};
 
     for (int i = 0; i < 10; ++i)
-        add_new_node(tree, random() % 10);
+        addNode(tree, random() % 10);
 
-    add_new_node(tree, 4);
-
-    print_tree(tree);
+    addNode(tree, 4);
+    printTree(tree);
 
     std::vector<std::string> path;
-    int result = treeSearch(tree, 5, path);
+    int result = searchTree(tree, 5, path);
 
     std::cout << result;
     for (const auto &step : path)
