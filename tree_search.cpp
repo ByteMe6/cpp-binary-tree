9e247dc9b52a99ce30f618b39aeee39c2748025d
ByteMe6 <120343461+ByteMe6@users.noreply.github.com>
2026-06-18 01:40:25 +0300

    refactor: remove dead code and stale comments from tree_search.cpp

--- tree_search.cpp
+++ tree_search.cpp
@@ -1,37 +1,18 @@
-#include <iostream>
-#include <vector>
-#include <string>
 #include "treeStruct.h"
 
-using namespace std;
-
-int treeSearch(Tree *tree, int search, vector<string> &counter) {
-    //@g++-ignore segfault
-
+int treeSearch(Tree *tree, int search, std::vector<std::string> &path) {
     if (tree == nullptr) {
-        counter = {"nothing"};
-
+        path = {"nothing"};
         return -1;
-        // throw runtime_error("Tree is empty");
     }
 
-    // if (tree != NULL) {
     if (tree->root == search) {
-        // cout << tree->root << endl;
-
         return tree->root;
-    } else if (tree->root > search) {
-        counter.push_back("left");
-        return treeSearch(tree->left, search, counter);
-    } else if (tree->root < search) {
-        counter.emplace_back("right");
-
-        return treeSearch(tree->right, search, counter);
+    } else if (search < tree->root) {
+        path.push_back("left");
+        return treeSearch(tree->left, search, path);
     } else {
-        counter = {"nothing"};
-        return -1;
+        path.push_back("right");
+        return treeSearch(tree->right, search, path);
     }
-    // } else {
-    //     cerr << "Tree is empty" << endl;
-    // }
 }
